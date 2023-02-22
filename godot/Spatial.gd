717c660d1428acc769ff09913a67b0db7320d5b9
Kimmymochi <57956184+Kimmymochi@users.noreply.github.com>
2023-02-22 21:46:30 +0100

    god pretty good

---
+++ godot/Spatial.gd
@@ -0,0 +1,30 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_button_down():
+	
+	pass # Replace with function body.
+
+
+func _on_Button_toggled(button_pressed):
+	if button_pressed:
+		$kjoeb/kjoeb_animate.play("speen")
+	else:
+		$kjoeb/kjoeb_animate.stop(false)
+	
+	pass # Replace with function body.
