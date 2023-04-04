74fdb2ab8e401bac2a091f1f5fd5ac0c5931e0f9
Kimmymochi <57956184+Kimmymochi@users.noreply.github.com>
2023-04-05 00:20:16 +0200

    move research stuff

---
+++ ResearchTests/godot/Spatial.gd
@@ -0,0 +1,41 @@
+extends Spatial
+
+
+#export var r=1
+#var theta=0
+#var dtheta=2*PI/250
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+#func _process(_delta):
+#	$kjoeb.transform = rotateAround($Camera.transform, $kjoeb.transform)
+#	pass
+
+#func rotateAround(var a, var b):
+#	theta += dtheta
+#	b.origin.x = a.origin.x + r*cos(theta)
+#	b.origin.y = a.origin.y + r*sin(theta)
+#	b.origin.z = a.origin.z
+#
+func _on_Button_button_down():
+	
+	pass # Replace with function body.
+
+
+func _on_Button_toggled(button_pressed):
+	if button_pressed:
+		$Sun/Planet/PlanetAnimate.play("spin")
+		$Sun/Planet2/PlanetAnimate.play("spin")
+		
+		#$Earth.transform = rotateAround($Sun.transform, $Earth.transform)
+
+	else:
+		$Sun/Planet/PlanetAnimate.stop(false)
+		$Sun/Planet2/PlanetAnimate.stop(false)
+		
+	
+	
+	pass # Replace with function body.
