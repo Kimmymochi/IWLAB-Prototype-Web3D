c9aca0c9e06c455cffcc3981c8067ab50cf3eebb
Kimmymochi <57956184+Kimmymochi@users.noreply.github.com>
2023-04-05 00:23:35 +0200

    new Project for Godot 4

--- godot/Spatial.gd
+++ godot/Spatial.gd
@@ -1,21 +1,25 @@
-extends Spatial
+extends Node3D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+#export var r=1
+#var theta=0
+#var dtheta=2*PI/250
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+#func _process(_delta):
+#	$kjoeb.transform = rotateAround($Camera.transform, $kjoeb.transform)
 #	pass
 
-
+#func rotateAround(var a, var b):
+#	theta += dtheta
+#	b.origin.x = a.origin.x + r*cos(theta)
+#	b.origin.y = a.origin.y + r*sin(theta)
+#	b.origin.z = a.origin.z
+#
 func _on_Button_button_down():
 	
 	pass # Replace with function body.
@@ -23,8 +27,10 @@ func _on_Button_button_down():
 
 func _on_Button_toggled(button_pressed):
 	if button_pressed:
-		$kjoeb/kjoeb_animate.play("speen")
+		$Sun/Planet/PlanetAnimate.play("spin")
+		#$Earth.transform = rotateAround($Sun.transform, $Earth.transform)
+
 	else:
-		$kjoeb/kjoeb_animate.stop(false)
+		$Sun/Planet/PlanetAnimate.stop(false)
 	
 	pass # Replace with function body.
